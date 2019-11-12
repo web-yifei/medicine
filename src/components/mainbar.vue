bcdc8b227ba300d8008ccee4b1d60d6eb86911b1
web-yifei <cyf980608@163.com>
2019-11-12 12:33:48 +0800

    hyhdsb

--- src/components/mainbar.vue
+++ src/components/mainbar.vue
@@ -10,20 +10,62 @@
                 <p>春季饮食调养以养肝、清肝、入肝经食物为主。适当食用</p>
             </dt>
         </dl>
+
+          <dl>
+            <dd>
+                <img src="" alt="">
+            </dd>
+            <dt>
+                <h4>[春季养生原则]养肝为主 注意防风</h4>
+                <p>春季饮食调养以养肝、清肝、入肝经食物为主。适当食用</p>
+            </dt>
+        </dl>
+
+          <dl>
+            <dd>
+                <img src="" alt="">
+            </dd>
+            <dt>
+                <h4>[春季养生原则]养肝为主 注意防风</h4>
+                <p>春季饮食调养以养肝、清肝、入肝经食物为主。适当食用</p>
+            </dt>
+        </dl>
+
+          <dl class="last">
+            <dd>
+                <img src="" alt="">
+            </dd>
+            <dt>
+                <h4>[春季养生原则]养肝为主 注意防风</h4>
+                <p>春季饮食调养以养肝、清肝、入肝经食物为主。适当食用</p>
+            </dt>
+        </dl>
     </div>
 </template>
 <style lang="scss" scoped>
     dl{
         height:1rem;
+        width: 100%;
         dd{
             float:left;
             height:.5rem;
-            width: .93rem;
+            width: 1rem;
             background:#ff0;
+            margin-left: .1rem;
+            margin-top: .2rem;
         }
         dt{
             float:left;
-
+            height: 1rem;
+            width: 2.5rem;
+            float: right;
+            h4{
+                margin-bottom: .2rem;
+            }
+           
         }
+    }
+    .last{
+        margin-bottom: .5rem;
     }
 </style>
