361030a7ba733c2bfc828e18b9b0fc3a1060be38
web-yifei <cyf980608@163.com>
2019-11-13 11:40:48 +0800

    ‘cyf’

--- src/store/index.js
+++ src/store/index.js
@@ -5,31 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-<<<<<<< HEAD
     my_title:{
       headPic:'',
       headName:'',
-      Id:''
+      Id:'',
     },
+    isTabbarShow: true
   },
   mutations: {
     getMyInfo(state,params){
       state.my_title.headPic = params.pic
       state.my_title.headName = params.username
       state.my_title.Id = params.id
-    }
-=======
-    isTabbarShow:true
-  },
-  mutations: {
+    },
     showTabbar (state) {
       state.isTabbarShow = true
     },
     hideTabbar (state) {
       state.isTabbarShow = false
     },
->>>>>>> zdp
   },
+
   actions: {
     async getMyInfoAction(store){
       const userid =await Axios({url:'/api/token/validate', headers:{Authorization: localStorage.getItem('token')}})
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,12 +5,59 @@
        <span @click="handleClick" class="iconfont icon-back"></span>
        <!-- <router-link to="/calc"><button>结算</button></router-link> -->
       </header>
+        <h4>购物车</h4>
+        <h4 class="thing">共三件物品</h4>
+      <nav>
       <main>
         <input type="radio">
-        <div></div>
+        <div>
+           <img src="" alt="">
+           <p>长白山人参东北吉林特产长白山人参东北吉林特产</p>
+           <span>￥99</span>
+           <div>
+           <button class="dec">-</button>
+           <span>3</span>
+           <button class="add">+</button>
+           </div>
+        </div>
       </main>
 
-      <footer></footer>
+       <main>
+        <input type="radio">
+        <div>
+           <img src="" alt="">
+           <p>长白山人参东北吉林特产长白山人参东北吉林特产</p>
+           <span>￥99</span>
+           <div>
+           <button class="dec">-</button>
+           <span>3</span>
+           <button class="add">+</button>
+           </div>
+        </div>
+      </main>
+
+       <main>
+        <input type="radio">
+        <div>
+           <img src="" alt="">
+           <p>长白山人参东北吉林特产长白山人参东北吉林特产</p>
+           <span>￥99</span>
+           <div>
+           <button class="dec">-</button>
+           <span>3</span>
+           <button class="add">+</button>
+           </div>
+        </div>
+      </main>
+      </nav>
+      <footer>
+          <div>
+             <input type="radio">
+             <span>全选</span>
+          </div>
+          <p>合计： <span>￥</span>0</p>
+          <router-link to="/calc" tag="button">结算</router-link>
+      </footer>
     </div>
 </template>
 <script>
@@ -19,10 +66,18 @@ export default {
     handleClick () {
         this.$router.back();
     }
-  }
+  },
+  beforeMount () { 
+     this.$store.commit('hideTabbar')
+  },
+  beforeDestroy () { 
+     this.$store.commit('showTabbar')
+  },
 }
 </script>
 <style lang="scss" scoped>
+   body{
+      position: relative;
     header{
       height: 1.5rem;
       background: rgb(236, 84, 89);
@@ -37,31 +92,182 @@ export default {
         margin-left: .1rem;
       }
     }
-    main{
-      height: 1.5rem;
-      width: 90%;
-      margin: 0 auto;
-      background: #f0f;
+   
+    footer{
+      height: .5rem;
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background: white;
+      border-top: 1px solid #ccc;
+      div{
+        float: left;
+        width: 1rem;
+        height: .5rem;
+         input{
+          position: absolute;
+          left: .4rem;
+          top: .2rem;
+         }
+         span{
+           display: block;
+           position: absolute;
+           left: .6rem;
+           top: .16rem;
+         }
+      }
+      p{
+        float: left;
+        width: 1rem;
+        height: .5rem;
+        text-align: center;
+        line-height: .5rem;
+        margin-left: .8rem;
+        span{
+          color: rgb(236, 84, 89);
+        }
+      }
+      button{
+        float: right;
+        width: .74rem;
+        height: .3rem;
+        text-align: center;
+        line-height: .3rem;
+        margin-right: .2rem;
+        margin-top: .10rem;
+        padding: 0;
+        border: 0;
+        border-radius: .08rem;
+        color: white;
+        font-size: .15rem;
+        background: rgb(236, 84, 89);
+      }
+    }
+    h4{
+      position: absolute;
+      top:.3rem;
+      right: .3rem;
+      color: white;
+    }
+    .thing{
+      top: .6rem;
+    }
+
+    nav{
       position: fixed;
+      // background: #ccc;
+      width:90%;
       top: 1rem;
       left: .18rem;
-      border-radius: .1rem;
+      main{
+        height: 1.5rem;
+        width: 100%;
+        margin: 0 auto;
+        margin-bottom: .2rem;
+        background: white;
+        border-radius: .1rem;
+        border: 1px solid #ccc;
+        position: relative;
       input{
         border: 0;
         position: absolute;
         left: .2rem;
         top: .7rem;
         background: white;
-      }
-      div{
+        }
+        div{
         width:2.5rem;
         height: 100%;
-        background: #0ff; 
+        // background: #0ff; 
         float: right;
+        position: relative;
+          img{
+            width: .6rem;
+            height: .6rem;
+            background: #000;
+            position: absolute;
+            top: .2rem;
+            left: 0;
+          }
+          p{
+            position: absolute;
+            top: .2rem;
+            left: .7rem;
+          }
+          span{
+            color: rgb(236, 84, 89);
+            position: absolute;
+            top: .9rem;
+            left: 0;
+            font-size: .16rem;
+          }
+         div{
+           width: 1.3rem;
+           height: .3rem;
+           position: absolute;
+           bottom: .2rem;
+           right: .6rem;
+           text-align: center;
+           line-height: .3rem;
+           .dec{
+             position: relative;
+             left: 0;
+             top: 0;
+             display: block;
+             width: .5rem;
+             height: .3rem;
+             line-height: .3rem;
+             border: 0;
+             border-top: 1px solid #ccc;
+             border-left: 1px solid #ccc;
+             border-bottom: 1px solid #ccc;
+             padding: 0;
+             font-size: .2rem;
+             border-top-left-radius: .1rem;
+             border-bottom-left-radius: .1rem;
+             background: white;
+             color: black;
+           }
+            span{
+            position: absolute;
+            left: .5rem;
+            top: 0;
+            padding: 0;
+            margin: 0;
+            outline: none;
+            border: 0;
+            border: 1px solid #ccc;
+            width: .3rem;
+            height: .3rem;
+            background: white;
+             color: black;
+
+          }
+          .add{
+             position: relative;
+             left: .8rem;
+             top: -.3rem;
+             display: block;
+             width: .5rem;
+             height: .3rem;
+             line-height: .3rem;
+             border: 0;
+             border-top: 1px solid #ccc;
+             border-bottom: 1px solid #ccc;
+             border-right: 1px solid #ccc;
+             padding: 0;
+             text-align: center;
+             border-bottom-right-radius: .1rem;
+             border-top-right-radius: .1rem;
+             background: white;
+             color: black;
+             font-size: .15rem;
+             
+          }
+         }
+      }
       }
     }
-    footer{
-      height: .5rem;
-      border-top: 1px solid #ccc;
-    }
+  }
 </style>
